0be3cddf0c1d05516062f63961c32ae0e6aaf66a
ty-hongjy <hongjy1@163.com>
2017-04-01 09:33:12 +0800

    0401

--- ex1.m
+++ ex1.m
@@ -4,3 +4,18 @@ plot(x,y1,x,y,'--r',x,-y,'--r')
 legend('exp(-0.5x)sin(5x)','exp(-0.5x)')
 title("My first figure")
 #ezplot('exp(-0.5*x)*sin(5*x)',[0,10,-1,1])
+
+
+theta=0:0.001:2*pi;
+r=cos(3*theta)
+polar(theta,r,'k')
+
+#bar,bar3
+x=-2.9:0.2:2.9
+y=exp(-x.*x)
+subplot(1,2,1)
+bar(x,y)
+title('figure£º2-D Bar Chart')
+subplot(1,2,2)
+bar3(x,y)
+title('Figure:3-D Bar Chart')
