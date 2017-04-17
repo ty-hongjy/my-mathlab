83e66a66b36080eda15752b7b3e7b56512b1c159
ty-hongjy <hongjy1@163.com>
2017-04-17 16:22:35 +0800

    0417

--- ex3.m
+++ ex3.m
@@ -1,12 +1,12 @@
-x=-4:4
-y1=2*x+3;y2=2*x-3;
-plot(x,y1,x,y2,'--r')
-#legend('exp(-0.5x)sin(5x)','exp(-0.5x)')
-title("My first figure")
+%x=-4:4
+%y1=2*x+3;y2=2*x-3;
+%plot(x,y1,x,y2,'--r')
+%#legend('exp(-0.5x)sin(5x)','exp(-0.5x)')
+%title("My first figure")
 
 
-
-x=-4:4
+Xsacle =  1
+x=-4:0.2:4
 #axis([-4 -4 4 4])
 #y=[x;2*x+3;2*x-3;x=4;y=3]
 y=[2*x+3;2*x-3]
@@ -25,6 +25,12 @@ y2=[3,3]
 x3=[4 4]
 y3=[3 0]
 plot(x1,y1,x2,y2,x3,y3,'b')
+
+text(-0.3, -0.2,"O")
+text(-0.3,3.2,"A")
+text(4.2,3.2,"B")
+text(4.2,-0.2,"C")
+text(4.2,1.5,"P")
 
 %hold on 
 %plot(x3,y3,'g')
